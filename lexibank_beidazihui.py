52c3b17dccb1711dc5cabed63537d60e8cc492b8
MacyL <wumeishin@gmail.com>
2020-03-02 11:49:00 +0100

    update the python script

--- lexibank_beidazihui.py
+++ lexibank_beidazihui.py
@@ -1,19 +1,20 @@
 import attr
 import lingpy
-from clldutils.path import Path
-from clldutils.text import strip_chars, split_text_with_context
-from clldutils.misc import lazyproperty
+from pathlib import Path
+from clldutils.misc import slug
+from pylexibank import Concept, Language
+from pylexibank.dataset import Dataset as BaseDataset
+from pylexibank.forms import FormSpec
+from pylexibank.util import progressbar, getEvoBibAsBibtex
+
 from lingpy.sequence.sound_classes import syllabify
-from pylexibank.dataset import Concept, Language
-from pylexibank.dataset import NonSplittingDataset as BaseDataset
-from pylexibank.util import pb, getEvoBibAsBibtex
 
 @attr.s
-class BDConcept(Concept):
+class CustomConcept(Concept):
     Chinese = attr.ib(default=None)
 
 @attr.s
-class HLanguage(Language):
+class CustomLanguage(Language):
     Latitude = attr.ib(default=None)
     Longitude = attr.ib(default=None)
     ChineseName = attr.ib(default=None)
@@ -24,39 +25,46 @@ class HLanguage(Language):
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "beidazihui"
-    concept_class = BDConcept
-    language_class = HLanguage
-
+    concept_class = CustomConcept
+    language_class = CustomLanguage
+    form_spec = FormSpec(
+          missing_data=[''],
+          separators=";/,",
+          brackets={'(': ')', '[': ']'},
+          strip_inside_brackets=True,
+          first_form_only=True
+      )
+    # delet or update???
     def cmd_download(self, **kw):
-        self.raw.write("sources.bib", getEvoBibAsBibtex("Zihui", **kw))
+        self.raw_dir.write("sources.bib", getEvoBibAsBibtex("Zihui", **kw))
 
-    def cmd_install(self, **kw):
-        wl = lingpy.Wordlist(self.raw.posix("wordlist.tsv"))
-
-        with self.cldf as ds:
-            ds.add_sources(*self.raw.read_bib())
-            concepts = {}
-            for concept in self.concepts:
-                if concept['ENGLISH']:
-                    ds.add_concept(
-                            ID=concept['NUMBER'],
-                            Name=concept['ENGLISH'],
-                            Chinese=concept['CHINESE'],
-                            Concepticon_ID=concept['CONCEPTICON_ID'],
-                            Concepticon_Gloss=concept['CONCEPTICON_GLOSS']
-                            )
-                    concepts[concept['CHINESE']] = concept['NUMBER']
-
-            langs = {k['Name']: k['ID'] for k in self.languages}
-            ds.add_languages()
-
-            for k in pb(wl, desc="wl-to-cldf", total=len(wl)):
-                if wl[k, "value"] and wl[k, 'concept'] in concepts:
-                    ds.add_form_with_segments(
-                        Language_ID=langs[wl[k, "doculect"]],
-                        Parameter_ID=concepts[wl[k, 'concept']],
-                        Value=wl[k, "value"],
-                        Form=wl[k, "form"],
-                        Segments=wl[k, 'tokens'],
-                        Source="Zihui",
-                    )
+    def cmd_makecldf(self, args):
+        # add sources
+        args.writer.add_sources()
+        # read in data
+        ds = self.raw_dir / 'wordlist.tsv'
+        wl = lingpy.Wordlist(ds.as_posix())
+        # add concepts
+        concepts = {}
+        for concept in self.conceptlists[0].concepts.values():
+            args.writer.add_concept(
+                ID='_'.join([concept.number, slug(concept.gloss)]),
+                Name=concept.gloss,
+                Concepticon_ID=concept.concepticon_id,
+                Concepticon_Gloss=concept.concepticon_gloss,
+                Chinese=concept.attributes["chinese"]
+            )
+            concepts[concept.attributes["chinese"]] = '_'.join([concept.number, slug(concept.gloss)])
+        # add languages
+        languages = args.writer.add_languages(lookup_factory="Name")
+        # add forms
+        for idx in progressbar(wl, desc = 'cldfify the data'):
+            cogid = idx
+            if wl[idx, 'concept'] in concepts.keys():
+                args.writer.add_form_with_segments(
+                    Language_ID=languages[wl[idx, 'doculect']],
+                    Parameter_ID=concepts[wl[idx, 'concept']],
+                    Value=wl[idx, 'value'],
+                    Form=wl[idx, 'form'],
+                    Segments=wl[idx, 'tokens']
+                )
