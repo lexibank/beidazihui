15e3a15482fbace5335e7b3aca9aec25d3438d9f
MacyL <wumeishin@gmail.com>
2020-03-02 14:07:02 +0100

    source concept list from the etc/ folder

--- lexibank_beidazihui.py
+++ lexibank_beidazihui.py
@@ -18,8 +18,8 @@ class CustomLanguage(Language):
     Latitude = attr.ib(default=None)
     Longitude = attr.ib(default=None)
     ChineseName = attr.ib(default=None)
-    SubGroup = attr.ib(default='Sinitic')
-    Family = attr.ib(default='Sino-Tibetan')
+    SubGroup = attr.ib(default="Sinitic")
+    Family = attr.ib(default="Sino-Tibetan")
     DialectGroup = attr.ib(default=None)
 
 class Dataset(BaseDataset):
@@ -28,9 +28,9 @@ class Dataset(BaseDataset):
     concept_class = CustomConcept
     language_class = CustomLanguage
     form_spec = FormSpec(
-          missing_data=[''],
+          missing_data=[""],
           separators=";/,",
-          brackets={'(': ')', '[': ']'},
+          brackets={"(": ")", "[": "]"},
           strip_inside_brackets=True,
           first_form_only=True
       )
@@ -42,29 +42,30 @@ class Dataset(BaseDataset):
         # add sources
         args.writer.add_sources()
         # read in data
-        ds = self.raw_dir / 'wordlist.tsv'
+        ds = self.raw_dir / "wordlist.tsv"
         wl = lingpy.Wordlist(ds.as_posix())
         # add concepts
-        concepts = {}
-        for concept in self.conceptlists[0].concepts.values():
+        concepts_dict = {}
+        for concept in self.concepts:
             args.writer.add_concept(
-                ID='_'.join([concept.number, slug(concept.gloss)]),
-                Name=concept.gloss,
-                Concepticon_ID=concept.concepticon_id,
-                Concepticon_Gloss=concept.concepticon_gloss,
-                Chinese=concept.attributes["chinese"]
+                ID="_".join([concept["NUMBER"], slug(concept["ENGLISH"])]),
+                Name=concept["ENGLISH"],
+                Concepticon_ID=concept["CONCEPTICON_ID"],
+                Concepticon_Gloss=concept["CONCEPTICON_GLOSS"],
+                Chinese=concept["CHINESE"]
             )
-            concepts[concept.attributes["chinese"]] = '_'.join([concept.number, slug(concept.gloss)])
+            concepts_dict[concept["CHINESE"]] = "_".join([concept["NUMBER"], slug(concept["ENGLISH"])])
         # add languages
         languages = args.writer.add_languages(lookup_factory="Name")
         # add forms
-        for idx in progressbar(wl, desc = 'cldfify the data'):
+        for idx in progressbar(wl, desc = "cldfify the data"):
             cogid = idx
-            if wl[idx, 'concept'] in concepts.keys():
+            if wl[idx, "concept"] in concepts_dict.keys() and wl[idx, "doculect"] in languages.keys():
                 args.writer.add_form_with_segments(
-                    Language_ID=languages[wl[idx, 'doculect']],
-                    Parameter_ID=concepts[wl[idx, 'concept']],
-                    Value=wl[idx, 'value'],
-                    Form=wl[idx, 'form'],
-                    Segments=wl[idx, 'tokens']
+                    Language_ID=languages[wl[idx, "doculect"]],
+                    Parameter_ID=concepts_dict[wl[idx, "concept"]],
+                    Value=wl[idx, "value"],
+                    Form=wl[idx, "form"],
+                    Segments=wl[idx, "tokens"],
+                    Source="Zihui"
                 )
